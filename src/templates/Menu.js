d1a0270516280ef0708b71a59f74b33cf77b14e5
Anderson Ribeiro <98779877+AndersonRSouza@users.noreply.github.com>
2023-12-11 21:32:30 -0300

    menu

--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -11,25 +11,31 @@ import { useNavigate } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { useAuth } from "../componentes/auth/auth";
 import '../estilos/menu.css'
+>>>>>>> 4fde0d9acdb7cac585aba60af7c4bda4dc9fd1e5
 import { IconeLogout } from "../icones/icones";
 
 export default function Menu({ isAuthenticated }) {
   const { user } = useAuth();
   const auth = useAuth();
+<<<<<<< HEAD
   let permiteAcessar = user.perfil === "Administrador";
 
-
+=======
+>>>>>>> 4fde0d9acdb7cac585aba60af7c4bda4dc9fd1e5
   // const navigate = useNavigate();
   // const redirectPath = "login";
   // console.log(JSON.parse(localStorage.getItem("dadosUsuario")));
   // navigate(redirectPath, { replace: true });
   return (
+<<<<<<< HEAD
     <Navbar className="custom-navbar" variant="dark" expand="lg">
+=======
     <Navbar
       className="custom-navbar"
       variant="dark"
       expand="lg"
     >
+>>>>>>> 4fde0d9acdb7cac585aba60af7c4bda4dc9fd1e5
       <Container>
         <LinkContainer to="/">
           <Navbar.Brand>Menu</Navbar.Brand>
@@ -43,6 +49,7 @@ export default function Menu({ isAuthenticated }) {
             <LinkContainer to="/cadastroAcomodacao">
               <Nav.Link eventKey="cadastroAcomodacao">Acomodações</Nav.Link>
             </LinkContainer>
+<<<<<<< HEAD
             {permiteAcessar && (
               <>
                 <LinkContainer to="/cadastroFornecedor">
@@ -64,12 +71,14 @@ export default function Menu({ isAuthenticated }) {
             <LinkContainer to="/cadastroPedidoReserva">
               <Nav.Link eventKey="cadastroPedidoReserva">
                 Pedido de Reserva
+=======
             <LinkContainer to="/cadastroFornecedor">
               <Nav.Link eventKey="cadastroFornecedor">Fornecedores</Nav.Link>
             </LinkContainer>
             <LinkContainer to="/cadastroPedidoCompra">
               <Nav.Link eventKey="cadastroPedidoCompra">
                 Pedido de Compra
+>>>>>>> 4fde0d9acdb7cac585aba60af7c4bda4dc9fd1e5
               </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/cadastroProduto">
@@ -78,6 +87,7 @@ export default function Menu({ isAuthenticated }) {
             <LinkContainer to="/cadastroServico">
               <Nav.Link eventKey="cadastroServico">Servicos</Nav.Link>
             </LinkContainer>
+<<<<<<< HEAD
             {permiteAcessar && (
               <>
                 <LinkContainer to="/cadastroUsuario">
@@ -85,9 +95,11 @@ export default function Menu({ isAuthenticated }) {
                 </LinkContainer>
               </>
             )}
+=======
             <LinkContainer to="/cadastroUsuario">
               <Nav.Link eventKey="cadastroUsuario">Usuários</Nav.Link>
             </LinkContainer>
+>>>>>>> 4fde0d9acdb7cac585aba60af7c4bda4dc9fd1e5
             <LinkContainer to="/consumoProduto">
               <Nav.Link eventKey="consumoProduto">Consumo Produto</Nav.Link>
             </LinkContainer>
@@ -102,15 +114,17 @@ export default function Menu({ isAuthenticated }) {
               </LinkContainer>
             )}
             <LinkContainer to="/logout">
+<<<<<<< HEAD
               <Navbar.Brand>
                 <IconeLogout />
                 Logout
               </Navbar.Brand>
-
+=======
               <Navbar.Brand><IconeLogout/>Logout</Navbar.Brand>
             </LinkContainer>
             <LinkContainer to="/ajuda">
               <Navbar.Brand>Ajuda</Navbar.Brand>
+>>>>>>> 4fde0d9acdb7cac585aba60af7c4bda4dc9fd1e5
             </LinkContainer>
             {/* <LinkContainer to="/ajuda">
               <Navbar.Brand>Ajuda</Navbar.Brand>
@@ -121,3 +135,54 @@ export default function Menu({ isAuthenticated }) {
     </Navbar>
   );
 }
+
+{
+  /* <Navbar
+  style={{
+    backgroundColor: "#0000CD",
+    color: "#fff",
+    textAlign: "center",
+    padding: "1px",
+    marginBottom: "10px",
+  }}
+  variant="dark"
+  expand="lg"
+> */
+}
+//   <Container>
+//     <LinkContainer to="/">
+//       <Navbar.Brand>Menu</Navbar.Brand>
+//     </LinkContainer>
+//     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//     <Navbar.Collapse id="basic-navbar-nav">
+//       <Nav className="mr-auto">
+//         <NavDropdown title="Cadastro" id="basic-nav-dropdown">
+//           <LinkContainer to="/cadastroHospede">
+//             <NavDropdown.Item>Hóspedes</NavDropdown.Item>
+//           </LinkContainer>
+//           <LinkContainer to="/cadastroAcomodacao">
+//             <NavDropdown.Item>Acomodações</NavDropdown.Item>
+//           </LinkContainer>
+//           <LinkContainer to="/cadastroFornecedor">
+//             <NavDropdown.Item>Fornecedores</NavDropdown.Item>
+//           </LinkContainer>
+//           <LinkContainer to="/cadastroPedidoCompra">
+//             <NavDropdown.Item>Pedido de Compra</NavDropdown.Item>
+//           </LinkContainer>
+//           <LinkContainer to="/cadastroProduto">
+//             <NavDropdown.Item>Produtos</NavDropdown.Item>
+//           </LinkContainer>
+//           <LinkContainer to="/cadastroServico">
+//             <NavDropdown.Item>Servicos</NavDropdown.Item>
+//           </LinkContainer>
+//           <LinkContainer to="/cadastroUsuario">
+//             <NavDropdown.Item>Usuários</NavDropdown.Item>
+//           </LinkContainer>
+//           <LinkContainer to="/consumoProduto">
+//             <NavDropdown.Item>Consumo Produto</NavDropdown.Item>
+//           </LinkContainer>
+//           <LinkContainer to="/consumoServico">
+//             <NavDropdown.Item>Consumo Serviços</NavDropdown.Item>
+//           </LinkContainer>
+//         </NavDropdown>
+//       </Nav>
